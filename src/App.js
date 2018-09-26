b4e01151f3687cadb3095cec4dde786f27a98158
ataucorp <akibt@springrainit.com>
2018-09-26 12:26:28 +0600

    initail commit

---
+++ src/App.js
@@ -0,0 +1,40 @@
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import { Layout, Header, Navigation, Drawer, Content } from "react-mdl";
+import { Link } from "react-router-dom";
+import "./App.css";
+import Main from "./components/main";
+
+class App extends Component {
+  render() {
+    return (
+      <div className="demo-big-content">
+        <Layout>
+          <Header title="Title" scroll>
+            <Navigation>
+              <Link to="/about">About</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/resume">Resume</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Header>
+          <Drawer title="Title">
+            <Navigation>
+              <Link to="/about">About</Link>
+              <Link to="/projects">Projects</Link>
+              <Link to="/resume">Resume</Link>
+              <Link to="/contact">Contact</Link>
+            </Navigation>
+          </Drawer>
+          <Content>
+            <div className="page-content">
+              <Main />
+            </div>
+          </Content>
+        </Layout>
+      </div>
+    );
+  }
+}
+
+export default App;
